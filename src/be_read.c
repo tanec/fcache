99de9d701f2ddb68a4c0bb1db49ec1cbac4bb608
nx <tanec@163.com>
2010-05-21 11:47:26 +0000

    be_read: uint64_t convert error; page_print: dump body

--- src/be_read.c
+++ src/be_read.c
@@ -1,10 +1,10 @@
 #include <string.h>
 #include "be_read.h"
 
-int16_t
-read(stream_t *s)
+uint8_t
+readbyte(stream_t *s)
 { // read 1 byte
-  int16_t ret = -1;
+  uint8_t ret = 0;
   if (s->pos <= s->len) {
     ret = *(s->data+((s->pos)++));
   }
@@ -21,95 +21,95 @@ readarr(stream_t *s, char *dest, size_t size)
 bool
 readbool(stream_t *s)
 {
-  return read(s) != 0 ? true : false;
+  return readbyte(s) != 0 ? true : false;
 }
 
 int8_t
 read8(stream_t *s)
 {
-  return (int8_t)read(s);
+  return (int8_t)readbyte(s);
 }
 
 int16_t
 read16(stream_t *s)
 {
-  int16_t ch1 = read(s);
-  int16_t ch2 = read(s);
+  int16_t ch1 = readbyte(s);
+  int16_t ch2 = readbyte(s);
   return (int16_t)((ch1 << 8) + (ch2 << 0));
 }
 
 int32_t
 read32(stream_t *s)
 {
-  int16_t ch1 = read(s);
-  int16_t ch2 = read(s);
-  int16_t ch3 = read(s);
-  int16_t ch4 = read(s);
+  int32_t ch1 = readbyte(s);
+  int32_t ch2 = readbyte(s);
+  int32_t ch3 = readbyte(s);
+  int32_t ch4 = readbyte(s);
   return (int32_t)((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
 }
 
 int64_t
 read64(stream_t *s)
 {
-  int64_t ch1 = read(s);
-  int64_t ch2 = read(s);
-  int64_t ch3 = read(s);
-  int64_t ch4 = read(s);
-  int16_t ch5 = read(s);
-  int16_t ch6 = read(s);
-  int16_t ch7 = read(s);
-  int16_t ch8 = read(s);
+  int64_t ch1 = readbyte(s);
+  int64_t ch2 = readbyte(s);
+  int64_t ch3 = readbyte(s);
+  int64_t ch4 = readbyte(s);
+  int64_t ch5 = readbyte(s);
+  int64_t ch6 = readbyte(s);
+  int64_t ch7 = readbyte(s);
+  int64_t ch8 = readbyte(s);
   return (int64_t)((int64_t)(ch1 << 56) +
-		   (int64_t)((ch2&255) << 48) +
-		   (int64_t)((ch3&255) << 40) +
-		   (int64_t)((ch4 &255)<< 32)+
-		   (int64_t)((ch5&255) << 24) +
-		   (int64_t)((ch6&255) << 16) +
-		   (int64_t)((ch7&255) << 8) +
-		   (int64_t)((ch8&255) << 0));
+                   (int64_t)((ch2&255) << 48) +
+                   (int64_t)((ch3&255) << 40) +
+                   (int64_t)((ch4 &255)<< 32)+
+                   (int64_t)((ch5&255) << 24) +
+                   (int64_t)((ch6&255) << 16) +
+                   (int64_t)((ch7&255) << 8) +
+                   (int64_t)((ch8&255) << 0));
 }
 
 uint8_t
 readu8(stream_t *s)
 {
-  return (uint8_t) read(s);
+  return (uint8_t) readbyte(s);
 }
 
 uint16_t
 readu16(stream_t *s)
 {
-  int16_t ch1 = read(s);
-  int16_t ch2 = read(s);
+  uint16_t ch1 = readbyte(s);
+  uint16_t ch2 = readbyte(s);
   return (uint16_t)((ch1 << 8) + (ch2 << 0));
 }
 
 uint32_t
 readu32(stream_t *s)
 {
-  int16_t ch1 = read(s);
-  int16_t ch2 = read(s);
-  int16_t ch3 = read(s);
-  int16_t ch4 = read(s);
+  uint32_t ch1 = readbyte(s);
+  uint32_t ch2 = readbyte(s);
+  uint32_t ch3 = readbyte(s);
+  uint32_t ch4 = readbyte(s);
   return (uint32_t)((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
 }
 
 uint64_t
 readu64(stream_t *s)
 {
-  int64_t ch1 = read(s);
-  int64_t ch2 = read(s);
-  int64_t ch3 = read(s);
-  int64_t ch4 = read(s);
-  int16_t ch5 = read(s);
-  int16_t ch6 = read(s);
-  int16_t ch7 = read(s);
-  int16_t ch8 = read(s);
-  return (int64_t)((int64_t)(ch1 << 56) +
-		   (int64_t)((ch2&255) << 48) +
-		   (int64_t)((ch3&255) << 40) +
-                   (int64_t)((ch4&255) << 32) +
-		   (int64_t)((ch5&255) << 24) +
-		   (int64_t)((ch6&255) << 16) +
-                   (int64_t)((ch7&255) << 8)  +
-		   (int64_t)((ch8&255) << 0));
+  uint64_t ch1 = readbyte(s);
+  uint64_t ch2 = readbyte(s);
+  uint64_t ch3 = readbyte(s);
+  uint64_t ch4 = readbyte(s);
+  uint64_t ch5 = readbyte(s);
+  uint64_t ch6 = readbyte(s);
+  uint64_t ch7 = readbyte(s);
+  uint64_t ch8 = readbyte(s);
+  return (uint64_t)((uint64_t)(ch1 << 56) +
+                   (uint64_t)((ch2&255) << 48) +
+                   (uint64_t)((ch3&255) << 40) +
+                   (uint64_t)((ch4&255) << 32) +
+                   (uint64_t)((ch5&255) << 24) +
+                   (uint64_t)((ch6&255) << 16) +
+                   (uint64_t)((ch7&255) << 8)  +
+                   (uint64_t)((ch8&255) << 0));
 }
--- src/reader.c
+++ src/reader.c
@@ -12,13 +12,28 @@ page_print(page_t *page)
   printf("<== page@%llx start:\n", page);
   printf("  head.version=%d\n", page->head.version);
   printf("  head.valid=%d\n", page->head.valid);
-  printf("  head.time_expire=%lld\n", page->head.time_expire);
-  printf("  head.time_create=%lld\n", page->head.time_create);
-  printf("  head.page_no=%lld\n", page->head.page_no);
+  printf("  head.time_expire=%lld(%llx)\n", page->head.time_expire, page->head.time_expire);
+  printf("  head.time_create=%lld(%llx)\n", page->head.time_create, page->head.time_create);
+  printf("  head.page_no=%lld(%llx)\n", page->head.page_no, page->head.page_no);
   printf("  head.type=%d\n", page->head.type);
   printf("  head.keyword=%s\n", page->head.keyword);
   printf("  head.ig=%s\n", page->head.ig);
   printf("  head.auth_type=%d\n", page->head.auth_type);
   printf("  head.param=%s\n", page->head.param);
+
+  printf("  page.body_len=%d(%04x)\n  page.body:\n", page->body_len, page->body_len);
+  unsigned char *b = (unsigned char *)page->body;
+  int i, n=128;
+  for(i=1; i<=n; i++){
+      printf("%02x", *(b+i-1));
+      if(i%32==0) { printf("\n"); continue; }
+      if(i>0 && i%2==0) printf(" ");
+  }
+  printf("...\n");
+  for(i=1; i<=n; i++){
+      printf("%02x", *(b+page->body_len-n+i-1));
+      if(i%32==0) { printf("\n"); continue; }
+      if(i%2==0) printf(" ");
+  }
   printf("<== page@%llx end.\n", page);
 }
