10552487bf3ffd346da8a98b65947be55596cbac
nx <tanec@163.com>
2010-06-01 19:02:30 +0000

    log

--- src/log.c
+++ src/log.c
@@ -11,6 +11,14 @@ void
 tlog(log_level_t level, const char *fmt, ...)
 {
   if (level >= cfg.log_level) {
+    if (!cfg.daemon) {
+      va_list args;
+      va_start(args, fmt);
+      vprintf(fmt, args);
+      va_end(args);
+      printf("\n");
+      return;
+    }
     if (log == NULL) {
       log = fopen(cfg.log_file, "a");
       if(log!=NULL) setvbuf(log, NULL, _IONBF, 0);
