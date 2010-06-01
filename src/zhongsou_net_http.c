f6ddd7d5634c2083627223b6168443ce5d8be50f
nx <tanec@163.com>
2010-06-02 01:11:12 +0800

    refactor

--- src/zhongsou_net_http.c
+++
@@ -1,7 +0,0 @@
-#include "zhongsou_net_http.h"
-
-void
-http_bypass(request_t *req, response_t *resp)
-{
-
-}
--- src/zhongsou_net_http.h
+++
@@ -1,8 +0,0 @@
-#ifndef ZHONGSOU_NET_HTTP_H
-#define ZHONGSOU_NET_HTTP_H
-
-#include "process.h"
-
-void http_bypass(request_t *, response_t *);
-
-#endif // ZHONGSOU_NET_HTTP_H
