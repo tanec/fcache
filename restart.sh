da181b427946e34ed67214f9c8bbcde10db35684
nx <tanec@163.com>
2010-07-22 14:51:17 +0000

    cfg&&script

---
+++ restart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+set -e
+
+F=`readlink -f $0`
+D=`dirname $F`
+
+function start() {
+  case $1 in
+  ishc)
+    $D/keeprun $D/ishc -i 0.0.0.0 ;;
+  fcache)
+    $D/keeprun $D/fcache -C $D/fcache.cfg ;;
+esac
+}
+
+pkill -9 $1
+ps -ef | grep -v grep | grep -E "keeprun .*$1" || start $1
