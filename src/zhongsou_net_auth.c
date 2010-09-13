3cc266d03753b1251c099be52f16ced22b77b080
root <root@slax>
2010-09-13 16:29:00 +0800

    auth log

--- src/zhongsou_net_auth.c
+++ src/zhongsou_net_auth.c
@@ -15,10 +15,10 @@ auth_http(const char *igid, const char *keyword, uint32_t auth_type, const char 
 
   svr = next_server_in_group(&cfg.auth);
   if (svr!=NULL && http_post(svr->url, &resp, 3, "igid", igid, "keyword", keyword, "auth", json)) {
-    tlog(DEBUG, "result=%s", resp.reply);
     // strlen("{"PageAuth":0,"GroupAuth":0}")=28
     if (resp.reply!=NULL && resp.size>15)
       ret = (*(resp.reply+12) != '0') && (*(resp.reply+26) != '0');
+    tlog(DEBUG, "result=%s %d", resp.reply, ret);
   }
   if (resp.reply != NULL) free(resp.reply);
   return ret;
