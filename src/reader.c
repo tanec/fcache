4feaefc65a4e5cca0349764c515c58dbdb20edcd
root <root@slax>
2010-09-13 16:26:10 +0800

    read static page: format time

--- src/reader.c
+++ src/reader.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <time.h>
 #include "reader.h"
 
+static inline void
+fmt_time(char * dest, size_t len, uint64_t milliseconds)
+{
+  struct tm *tm;
+  time_t tt = milliseconds/1000;
+  tm = localtime(&tt);
+  strftime(dest, len, "%F %T", tm);
+}
 
 void
 page_print(page_t *page)
 {
+#define TL  128
+  char tm[TL];
   if (page == NULL) {
     printf("page = NULL\n");
     return;
@@ -12,8 +23,12 @@ page_print(page_t *page)
   printf("<== page@%p start:\n", page);
   printf("  head.version=%d\n", page->head.version);
   printf("  head.valid=%d\n", page->head.valid);
-  printf("  head.time_expire=%lld(%llx)\n", page->head.time_expire, page->head.time_expire);
-  printf("  head.time_create=%lld(%llx)\n", page->head.time_create, page->head.time_create);
+  fmt_time(tm, TL, page->head.time_dead);
+  printf("  head.time_dead  =%lld(%llx)(%s)\n", page->head.time_dead,   page->head.time_dead,   tm);
+  fmt_time(tm, TL, page->head.time_expire);
+  printf("  head.time_expire=%lld(%llx)(%s)\n", page->head.time_expire, page->head.time_expire, tm);
+  fmt_time(tm, TL, page->head.time_create);
+  printf("  head.time_create=%lld(%llx)(%s)\n", page->head.time_create, page->head.time_create, tm);
   printf("  head.page_no=%lld(%llx)\n", page->head.page_no, page->head.page_no);
   printf("  head.type=%d\n", page->head.type);
   printf("  head.keyword=%s\n", page->head.keyword);
